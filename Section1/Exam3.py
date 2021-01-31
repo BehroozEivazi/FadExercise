bc5e26d4698cd025d6e84ac20d4105dfc6e3ed29
BehroozEivazi <behroozeivazi@yahoo.com>
2021-01-31 23:07:07 +0330

    bitwise image and mask using videoCapture and bitwise

---
+++ Section1/Exam3.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+
+mask = cv.imread('C:/Users/behrooz/PycharmProjects/VisionComputing/Section1/Mask.jpg')
+
+
+def grayToBinary(img, rang):
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img[img > rang] = 255
+    img[img <= rang] = 0
+    return img
+
+
+mask = grayToBinary(mask, 140)
+vid = cv.VideoCapture(0)
+
+while (True):
+
+    ret, frame = vid.read()
+    # frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # if we comment top line we should change bitwise function and we can change the gray scale  image to rgb
+    width = frame.shape[0]
+    height = frame.shape[1]
+    mask = cv.resize(mask, (height, width), interpolation=cv.INTER_AREA)
+    print('frame shpae is {}'.format(frame.shape))
+    print('mask shpae is {}'.format(mask.shape))
+    # result = cv.bitwise_and(frame, mask)
+    # top line for base image gray scale
+    result = cv.bitwise_and(frame,frame, mask=mask)
+    cv.imshow('frame', result)
+    if cv.waitKey(1) & 0xFF == ord('s'):
+        break
+
+vid.release()
